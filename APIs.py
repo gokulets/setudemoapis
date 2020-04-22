c1bc3b70deffeda9198b81e73d287cdcfb465153
Gokul Sengottuvelu <gsengott@akamai.com>
2020-04-22 13:11:15 +0530

    JWT implementation

--- APIs.py
+++ APIs.py
@@ -51,3 +51,84 @@ class Receipts(db.Model):
    amountPaid = db.Column(db.Integer, nullable=True)
    billAmount = db.Column(db.Integer, nullable=True)
 
+
+def token_required(f):  
+    @wraps(f)  
+    def decorator(*args, **kwargs):
+
+       token = None 
+    
+       if 'Authorization' in request.headers:
+           token = request.headers['Authorization'].split(" ")[1]
+
+       if not token:  
+          return jsonify({'message': 'a valid token is missing'})   
+
+
+       try:  
+          data = jwt.decode(token, app.config['SECRET_KEY']) 
+          print(data)
+          current_user = Users.query.filter_by(public_id=data['public_id']).first()  
+          print(current_user)
+       except:  
+          return jsonify({'message': 'token is invalid'})  
+
+       return f(current_user, data['public_id'], *args,  **kwargs)  
+    return decorator 
+
+@app.route('/register', methods=['GET', 'POST'])
+def signup_user():  
+ data = request.get_json()  
+
+ hashed_password = generate_password_hash(data['password'], method='sha256')
+ 
+ new_user = Users(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False) 
+ db.session.add(new_user)  
+ db.session.commit()    
+
+ return jsonify({'message': 'registered successfully'})   
+
+
+@app.route('/login', methods=['GET', 'POST'])  
+def login_user(): 
+ 
+  auth = request.authorization   
+
+  if not auth or not auth.username or not auth.password:  
+     return make_response('could not verify', 401, {'WWW.Authentication': 'Basic realm: "login required"'})    
+
+  user = Users.query.filter_by(name=auth.username).first()   
+     
+  if check_password_hash(user.password, auth.password):  
+     token = jwt.encode({'public_id': user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(days=30)}, app.config['SECRET_KEY'])  
+     data = jwt.decode(token, app.config['SECRET_KEY']) 
+     print(data)
+
+     return jsonify({'token' : token.decode('UTF-8')}) 
+
+  return make_response('could not verify',  401, {'WWW.Authentication': 'Basic realm: "login required"'})
+
+
+@app.route('/',methods=['GET','POST','PUT','DELETE'])
+def hello():
+
+ return jsonify({'ServerStatus':'ON','Message':'Hello World'})
+
+@app.route('/user', methods=['GET'])
+def get_all_users():  
+   
+   users = Users.query.all() 
+
+   result = []   
+
+   for user in users:   
+       user_data = {}   
+       user_data['public_id'] = user.public_id  
+       user_data['name'] = user.name 
+       user_data['password'] = user.password
+       user_data['admin'] = user.admin 
+       
+       result.append(user_data)   
+
+   return jsonify({'users': result})  
+
